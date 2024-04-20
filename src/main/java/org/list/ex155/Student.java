91767c0c192ed148cd0855affc76f56d8fd99448
chp320 <chp320@naver.com>
2024-04-20 23:30:29 +0900

    vector, hashtable 을 확인하기 위해 vector, hashtable 사용법 예제

---
+++ src/main/java/org/list/ex155/Student.java
@@ -0,0 +1,66 @@
+package org.list.ex155;
+
+public class Student implements Cloneable {
+    private String name;
+    private int id;
+    private String address;
+
+    public Student(String name, int id, String address) {
+        this.name = name;
+        this.id = id;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String toString() {
+        return name + " " + id + " " + address;
+    }
+
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            return this;
+        }
+    }
+
+    // 객체 비교를 하기 위해서는 equals와 hashCode가 필요하다.
+    public boolean equals(Object obj) {
+        boolean isE = false;
+        Student temp = (Student) obj;
+        if (temp.getName().equals(name) && temp.getId() == id && temp.getAddress().equals(address)) {
+            isE = true;
+        }
+        return isE;
+    }
+
+    public int hashCode() {
+        int temp = 0;
+        temp = name.hashCode() + address.hashCode() + id + 37;
+        return temp;
+    }
+
+}
